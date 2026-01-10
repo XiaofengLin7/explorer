c851b35d81d11674b88911453612706782a119d9
Yilei Chen <ylchen9@scc-210.scc.bu.edu>
2026-01-10 17:07:58 -0500

    create prompts folder to store different prompts

---
+++ prompts/prompt.py
@@ -0,0 +1,18 @@
+MineSweeper_reflection_prompt = """
+[Reflection] Now it’s your turn to reflect on the past experience and come up with a new plan of action.
+- Your response should first be step-by-step reasoning about the strategy and path you took
+  to attempt to complete the task. Identify where things went wrong or could be better.
+- Then devise a concise, new plan of action that accounts for your mistake with reference to
+  specific actions that you should have taken.
+- Finally, end the response with your reflection and improved plan inside <remark>
+  </remark> tags, to guide the next trial.
+"""
+
+# # Your Task
+# You will be given the history of a past experience.
+# Your job now is to **reflect on the past experience**, identify any **mistakes or inefficiencies**,
+# and then devise a **concise, improved plan** for your next try starting from the
+# original initial state.
+
+
+
