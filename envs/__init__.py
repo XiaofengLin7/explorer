22dadf5189cebe39582de77ae073371dca520fe9
XiaofengLin7 <xfl199801@gmail.com>
2026-01-16 00:13:24 -0500

    Environment maze not found in registry.

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,7 +3,8 @@
 from envs.gem_env_adapter import GEMEnvAdapter
 from envs.multi_episode_env import MultiEpisodeEnv
 from envs.blackjack_env_adapter import BlackjackEnvAdapter
-
+from envs.maze_env_adapter import MazeEnvAdapter
+from envs.rps_env_adapter import RockPaperScissorsEnvAdapter
 # Auto-register the only-reveal Minesweeper environment
 import envs.register_custom_minesweeper  # noqa: F401
 
@@ -11,6 +12,8 @@ __all__ = [
     "GEMEnvAdapter",
     "MultiEpisodeEnv",
     "BlackjackEnvAdapter",
+    "MazeEnvAdapter",
+    "RockPaperScissorsEnvAdapter",
 ]
 
 
